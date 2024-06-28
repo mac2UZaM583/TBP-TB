627a49c3dfa1c3e9a9659075b3f597234fb4aa07
mac2UZaM583 <163586801+mac2UZaM583@users.noreply.github.com>
2024-06-28 23:59:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ counter = itertools.count(start=0, step=1)
 with open('orderId.txt', 'r', encoding='utf-8') as f:
     orderId_copy = [f.read()]
 
-def get_info(closed_pnl_pos, order_id):
+def get_info(closed_pnl_pos):
     entry_price = float(closed_pnl_pos['avgEntryPrice'])
     exit_price = float(closed_pnl_pos['avgExitPrice'])
     leverage = float(closed_pnl_pos['leverage'])
@@ -24,6 +24,7 @@ def get_info(closed_pnl_pos, order_id):
     wallet_balance = round(float(balance_info['result']['list'][0]['coin'][0]['walletBalance']), 2)
     pnl_percent_none = round((((entry_price / exit_price) * 100) - 100) * leverage, 2)
     pnl_percent = -pnl_percent_none if closed_pnl_pos['side'] == 'Sell' else pnl_percent_none
+    time = session.get_order_history(category='linear')['result']['list'][0]['updatedTime']    
     return (closed_pnl_pos['symbol'], 
             leverage, 
             closed_pnl_pos['side'], 
@@ -31,21 +32,20 @@ def get_info(closed_pnl_pos, order_id):
             wallet_balance, 
             entry_price, 
             exit_price, 
-            pnl_percent)
+            pnl_percent,
+            time)
 
-def get_messege(symbol, leverage, side, closed_pnl, wallet_balance, entry_price, exit_price, pnl_percent, position_num):
-    messege = (
-                f"🌍 Ticker: {symbol}\n"
-                f"Leverage: {leverage}\n"
-                f"Side: {side}\n"
-                f"Closed PNL: {closed_pnl}\n"
-                f"Balance USDT: {wallet_balance}\n"
-                f"Entry price: {entry_price}\n"
-                f"Exit price: {exit_price}\n"
-                f"PNL percent: {pnl_percent}%\n"
-                f"Position number: {position_num}\n"
-                f'Time: {datetime.now()}'
-)
+def get_messege(symbol, leverage, side, closed_pnl, wallet_balance, entry_price, exit_price, pnl_percent, time, position_num):
+    messege = (f"🌍 Ticker: {symbol}\n" 
+               f"Leverage: {leverage}\n" 
+               f"Side: {side}\n" 
+               f"Closed PNL: {closed_pnl}\n" 
+               f"Balance USDT: {wallet_balance}\n" 
+               f"Entry price: {entry_price}\n" 
+               f"Exit price: {exit_price}\n" 
+               f"PNL percent: {pnl_percent}%\n" 
+               f'Time: {time}\n' 
+               f"Position number: {position_num}")
     return messege
 
 def save(position_num, order_id):
@@ -71,7 +71,7 @@ def main():
                 orderId_copy.clear()
                 position_num = str(position_num + 1)
                 orderId_copy.append(order_id)
-                info = get_info(closed_pnl_pos=closed_pnl_pos, order_id=order_id)
+                info = get_info(closed_pnl_pos=closed_pnl_pos)
                 messege = get_messege(*info, position_num=position_num)
                 send_message_to_channel(messege)
                 save(position_num=position_num, order_id=order_id)
